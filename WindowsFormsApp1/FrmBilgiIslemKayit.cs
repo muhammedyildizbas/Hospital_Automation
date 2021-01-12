d3881edfb2dfeceb81c1a44802c114fafb4f9f4f
muhammedyildiz3465@gmail.com <63956100+muhammedyildizbas@users.noreply.github.com>
2021-01-12 21:58:57 +0300

    Bilgi İşlem kaydı veri tabanına bağlandı

--- WindowsFormsApp1/FrmBilgiIslemKayit.cs
+++ WindowsFormsApp1/FrmBilgiIslemKayit.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace WindowsFormsApp1
 {
@@ -16,6 +17,7 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+        SqlConnection baglanti = new SqlConnection(@"Data Source=DESKTOP-44T2TND;Initial Catalog=Hospital_Automation;Integrated Security=True");
 
         private void label6_Click(object sender, EventArgs e)
         {
@@ -23,6 +25,39 @@ namespace WindowsFormsApp1
         }
 
         private void FrmBilgiIslemKayit_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                baglanti.Open();
+                SqlCommand komutKaydet = new SqlCommand("insert into BilgiIslem (TcNo,Adi,Soyadi,Telefon,EPosta,Sifre,Cinsiyet) values (@p1,@p2,@p3,@p4,@p5,@p6,@p7)", baglanti);
+                komutKaydet.Parameters.AddWithValue("@p1", TxtTc.Text);
+                komutKaydet.Parameters.AddWithValue("@p2", TxtBlgIslemAd.Text);
+                komutKaydet.Parameters.AddWithValue("@p3", TxtBlgIslemSoyad.Text);
+                komutKaydet.Parameters.AddWithValue("@p4", TxtBlgIslemTel.Text);
+                komutKaydet.Parameters.AddWithValue("@p5", TxtBlgIslemEPosta.Text);
+                komutKaydet.Parameters.AddWithValue("@p6", TxtBlgIslemSifre.Text);
+                komutKaydet.Parameters.AddWithValue("@p7", CmbCinsiyet.Text);
+                komutKaydet.ExecuteNonQuery();
+                MessageBox.Show("Kayıt Yapıldı");
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Bilgi İşlem Kaydı Yapılırken Hata");
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
