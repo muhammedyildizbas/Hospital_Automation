2c1e03824400548cf3d60fc72e0d70eed7a5e00b
muhammedyildiz3465@gmail.com <63956100+muhammedyildizbas@users.noreply.github.com>
2021-01-12 18:51:36 +0300

    Poliklinik Ekleme veri tabanı bağlantısı

--- WindowsFormsApp1/FrmPoliklinikEkle.Designer.cs
+++ WindowsFormsApp1/FrmPoliklinikEkle.Designer.cs
@@ -28,17 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TxtPoliklinik = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
+            this.btnPoliklinikEkle = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // TxtPoliklinik
             // 
-            this.textBox1.Location = new System.Drawing.Point(158, 52);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(169, 20);
-            this.textBox1.TabIndex = 10;
+            this.TxtPoliklinik.Location = new System.Drawing.Point(158, 52);
+            this.TxtPoliklinik.Name = "TxtPoliklinik";
+            this.TxtPoliklinik.Size = new System.Drawing.Size(169, 20);
+            this.TxtPoliklinik.TabIndex = 10;
             // 
             // label1
             // 
@@ -50,27 +50,29 @@
             this.label1.TabIndex = 9;
             this.label1.Text = "Poliklinik Adı:";
             // 
-            // button2
+            // btnPoliklinikEkle
             // 
-            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
-            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.button2.Location = new System.Drawing.Point(261, 82);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(66, 25);
-            this.button2.TabIndex = 26;
-            this.button2.Text = "Ekle";
-            this.button2.UseVisualStyleBackColor = false;
+            this.btnPoliklinikEkle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnPoliklinikEkle.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnPoliklinikEkle.Location = new System.Drawing.Point(261, 82);
+            this.btnPoliklinikEkle.Name = "btnPoliklinikEkle";
+            this.btnPoliklinikEkle.Size = new System.Drawing.Size(66, 25);
+            this.btnPoliklinikEkle.TabIndex = 26;
+            this.btnPoliklinikEkle.Text = "Ekle";
+            this.btnPoliklinikEkle.UseVisualStyleBackColor = false;
+            this.btnPoliklinikEkle.Click += new System.EventHandler(this.button2_Click);
             // 
-            // Form7
+            // FrmPoliklinikEkle
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(354, 120);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnPoliklinikEkle);
+            this.Controls.Add(this.TxtPoliklinik);
             this.Controls.Add(this.label1);
-            this.Name = "Form7";
+            this.Name = "FrmPoliklinikEkle";
             this.Text = "Poliklinik Ekle";
+            this.Load += new System.EventHandler(this.FrmPoliklinikEkle_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -78,8 +80,8 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox TxtPoliklinik;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnPoliklinikEkle;
     }
 }
