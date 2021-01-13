3896d1516f7d3d79a635d7b332c92cafab9c734b
muhammedyildiz3465@gmail.com <63956100+muhammedyildizbas@users.noreply.github.com>
2021-01-13 11:45:42 +0300

    Kullanıcının veri tabanı bağlantısı ile Tc ve Şifresi ile giriş yapması

--- WindowsFormsApp1/FrmKullaniciGiris.Designer.cs
+++ WindowsFormsApp1/FrmKullaniciGiris.Designer.cs
@@ -28,19 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.TxtSifre = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.TxtTcNo = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox2
+            // TxtSifre
             // 
-            this.textBox2.Location = new System.Drawing.Point(181, 93);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(153, 20);
-            this.textBox2.TabIndex = 14;
+            this.TxtSifre.Location = new System.Drawing.Point(181, 93);
+            this.TxtSifre.Name = "TxtSifre";
+            this.TxtSifre.Size = new System.Drawing.Size(153, 20);
+            this.TxtSifre.TabIndex = 14;
             // 
             // label2
             // 
@@ -48,16 +48,16 @@
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label2.Location = new System.Drawing.Point(29, 93);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(42, 18);
+            this.label2.Size = new System.Drawing.Size(102, 18);
             this.label2.TabIndex = 13;
-            this.label2.Text = "Şifre:";
+            this.label2.Text = "E-Devlet Şifre:";
             // 
-            // textBox1
+            // TxtTcNo
             // 
-            this.textBox1.Location = new System.Drawing.Point(181, 48);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(153, 20);
-            this.textBox1.TabIndex = 12;
+            this.TxtTcNo.Location = new System.Drawing.Point(181, 48);
+            this.TxtTcNo.Name = "TxtTcNo";
+            this.TxtTcNo.Size = new System.Drawing.Size(153, 20);
+            this.TxtTcNo.TabIndex = 12;
             // 
             // label1
             // 
@@ -79,6 +79,7 @@
             this.button2.TabIndex = 27;
             this.button2.Text = "Giriş";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // FrmKullaniciGiris
             // 
@@ -86,9 +87,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(363, 177);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.TxtSifre);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TxtTcNo);
             this.Controls.Add(this.label1);
             this.Name = "FrmKullaniciGiris";
             this.Text = "Kullanıcı Girişi";
@@ -99,9 +100,9 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox TxtSifre;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox TxtTcNo;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button2;
     }
